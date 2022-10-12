de1e090fc62620c1c2190a2bf35836fb3056f261
Roberto sanchez <robsanchez1772@gmail.com>
2022-10-11 18:07:22 -0700

    added sample code

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// sample variable initialize
+let username = "txtHacker";
+
+// reassignment
+username = "super sayain goku";
+
+// number
+const age = 0.2;
+
+// strings
+const name = "julian murillo";
+
+// char
+const letter = "c";
+
+// arrays
+// camelCasing vs pascalCasing
+//                     0      1         2
+const myFavCars = ["bengi", "julian", "renzo"];
+
+// forEach
+// anonymous/lambda
+myFavCars.forEach(function (value, index) {
+    console.log('we are at index: ', index)
+    console.log(value);
+});
+
+// push - add items to the array
+// myFavCars.push('renzo');
+// console.log(myFavCars)
+
+// pop - remove item in the array
+// myFavCars.pop();
+// console.log(myFavCars);
+
+// sort
+myFavCars.sort();
+
+
+// TODO:
+// map
+
+// TODO:
+// filter
+
+// TODO:
+// indexOf
+
+
+// Boolean
+const status = true;
+
+// objects
+const person = {
+ firstName: 'bengi',
+ lastName: 'depp',
+ age: null
+}
+
+person.age = person.age + 'hello';
+
+console.log(person);
+
+
+
