beb5fc076a58c183596fc738e04158c587f30260
Roberto sanchez <robsanchez1772@gmail.com>
2022-10-11 19:05:41 -0700

    added function samples

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+/**
+ * Roberto Sanchez
+ * github@gmail.com
+ * 
+ * This file shows samples fod ata types and
+ */
+
 // sample variable initialize
 let username = "txtHacker";
 
@@ -20,10 +27,10 @@ const myFavCars = ["bengi", "julian", "renzo"];
 
 // forEach
 // anonymous/lambda
-myFavCars.forEach(function (value, index) {
-    console.log('we are at index: ', index)
-    console.log(value);
-});
+// myFavCars.forEach(function (value, index) {
+//     console.log('we are at index: ', index)
+//     console.log(value);
+// });
 
 // push - add items to the array
 // myFavCars.push('renzo');
@@ -59,7 +66,86 @@ const person = {
 
 person.age = person.age + 'hello';
 
-console.log(person);
+// Operations
+
+// add
+const num1 = 10;
+const num2 = 20;
+
+// 30
+let total = num1 + num2;
+// ++  add 1
+// -- subtract
 
 
+// subtraction
+total = num1 - num2;
+
+// multiply
+total = num1 * num2;
+
+// divide 
+total = num1 / num2;
+
+
+// modulus
+total = 4 % 3;
+
+// conditional statement
+// if/else = true
+
+
+// const num1 = 10;
+// const num2 = 20;
+// == / === equals to
+// !==
+// if(num1 > num2){
+//     console.log('num1 is bigger')
+// } else if(num2 > num1){
+//     console.log('num2 is bigger')
+// }
+
+const myFavPeople = ["bengi", "julian", "renzo"];
+// console.log(myFavCars.length);
+// // loops
+// for(let i = 0; i < myFavPeople.length; i++){
+//     console.log(i);
+//     console.log(myFavCars[i])
+// }
+// console.log('alternate for loop')
+
+// for(let pizza of myFavPeople){
+//     console.log(pizza);
+// }
+
+/**
+ * Functions
+ */
+function joeMomma(){
+    console.log('hey she so ugly');
+}
+
+/**
+ * Takes two numbers and returns sum
+ */
+function sum(num1, num2){
+    return num1+num2;
+}
+
+
+console.log(tax)
+
+// holds the user's first name
+const name = 'dfsdf';
+
+/**
+ * This loops thoruhg car list and prints out each model name
+ */
+
+
+/**
+ * Checks if the age is atlesat 21
+ * 
+ * otherwise logs user out
+ */
 
